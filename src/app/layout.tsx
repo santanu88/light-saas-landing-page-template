cab70a0c3596b1a89aaa8d27b63d3a6767c27e2b
Shantanu Swain <137885216+santanu88@users.noreply.github.com>
2025-11-08 20:47:11 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,7 @@ import { twMerge } from "tailwind-merge";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
-};
+
 
 export default function RootLayout({
   children,
